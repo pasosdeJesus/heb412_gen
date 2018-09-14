fb834855143b42335aabc1b7b9b9c5dedfd6f532
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-09-14 10:31:23 -0500

    reservamos espacio para plantillas del sistema

---
+++ db/migrate/20180914153010_indice_plantillahcm.rb
@@ -0,0 +1,9 @@
+class IndicePlantillahcm < ActiveRecord::Migration[5.2]
+  def up
+    execute <<-SQL
+      SELECT setval('heb412_gen_plantillahcm_id_seq', 100);
+    SQL
+  end
+  def down
+  end
+end
