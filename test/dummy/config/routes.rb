f23a379da7f970f29c044bf1f9c4591931e56928
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-01-08 22:54:21 -0500

    Actualiza

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,30 +1,32 @@
 # encoding: UTF-8
 
 Rails.application.routes.draw do
+  scope '/heb412/' do 
 
-  devise_scope :usuario do
-    get 'sign_out' => 'devise/sessions#destroy'
-  end
-  devise_for :usuarios, :skip => [:registrations], module: :devise
-  as :usuario do
-    get 'usuarios/edit' => 'devise/registrations#edit', 
-      :as => 'editar_registro_usuario'    
-    put 'usuarios/:id' => 'devise/registrations#update', 
-      :as => 'registro_usuario'            
-  end
-  resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
+    devise_scope :usuario do
+      get 'sign_out' => 'devise/sessions#destroy'
+    end
+    devise_for :usuarios, :skip => [:registrations], module: :devise
+    as :usuario do
+      get 'usuarios/edit' => 'devise/registrations#edit', 
+        :as => 'editar_registro_usuario'    
+      put 'usuarios/:id' => 'devise/registrations#update', 
+        :as => 'registro_usuario'            
+    end
+    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
 
-  root 'sip/hogar#index'
-  mount Heb412Gen::Engine, at: "/", as: "heb412_gen"
-  mount Sip::Engine, at: "/", as: "sip"
+    root 'sip/hogar#index'
+    mount Heb412Gen::Engine, at: "/", as: "heb412_gen"
+    mount Sip::Engine, at: "/", as: "sip"
 
-  namespace :admin do
-    ab = ::Ability.new
-    ab.tablasbasicas.each do |t|
-      if (t[0] == "") 
-        c = t[1].pluralize
-        resources c.to_sym, 
-          path_names: { new: 'nueva', edit: 'edita' }
+    namespace :admin do
+      ab = ::Ability.new
+      ab.tablasbasicas.each do |t|
+        if (t[0] == "") 
+          c = t[1].pluralize
+          resources c.to_sym, 
+            path_names: { new: 'nueva', edit: 'edita' }
+        end
       end
     end
   end
