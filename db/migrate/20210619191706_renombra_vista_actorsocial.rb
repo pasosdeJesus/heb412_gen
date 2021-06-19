1ff23f67544c721e854f09ebd2a43dfbd5c92408
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-19 14:19:44 -0500

    Migracion para renombrar vista Actorsocial por Orgsocial en plantillas

---
+++ db/migrate/20210619191706_renombra_vista_actorsocial.rb
@@ -0,0 +1,18 @@
+class RenombraVistaActorsocial < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      UPDATE heb412_gen_plantillahcm
+        SET vista='Orgsocial' WHERE vista='Actorsocial';
+      UPDATE heb412_gen_plantillahcr
+        SET vista='Orgsocial' WHERE vista='Actorsocial';
+    SQL
+  end
+  def down
+    execute <<-SQL
+      UPDATE heb412_gen_plantillahcr
+        SET vista='Actorsocial' WHERE vista='Orgsocial';
+      UPDATE heb412_gen_plantillahcm
+        SET vista='Actorsocial' WHERE vista='Orgsocial';
+    SQL
+  end
+end
