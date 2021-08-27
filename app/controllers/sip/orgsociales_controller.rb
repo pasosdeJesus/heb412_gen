75208d1c8c093acd14cbcf6be80b07cdd2129630
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-08-27 15:24:42 -0500

    refactoriza

--- app/controllers/sip/orgsociales_controller.rb
+++ app/controllers/sip/orgsociales_controller.rb
@@ -1,8 +1,11 @@
-# encoding: UTF-8
 require_dependency "heb412_gen/concerns/controllers/orgsociales_controller"
 
 module Sip
   class OrgsocialesController < Heb412Gen::ModelosController
+
+    before_action :set_orgsocial, only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource class: Sip::Orgsocial
+
     include Heb412Gen::Concerns::Controllers::OrgsocialesController
   end
 end
--- test/dummy/app/controllers/sip/orgsociales_controller.rb
+++ test/dummy/app/controllers/sip/orgsociales_controller.rb
@@ -1,8 +1,9 @@
-# encoding: UTF-8
-
 require 'sip/concerns/controllers/orgsociales_controller'
 
 class Sip::OrgsocialesController < Heb412Gen::ModelosController
+
+  before_action :set_orgsocial, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource class: Sip::Orgsocial
 
   include Sip::Concerns::Controllers::OrgsocialesController
 
