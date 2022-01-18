0ee5177abd86e7566cba6c524ec00b0f8613576c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-18 16:14:52 -0500

    Actualiza

--- test/dummy/test/application_system_test_case.rb
+++ test/dummy/test/application_system_test_case.rb
@@ -2,17 +2,6 @@ require_relative "../../../test/test_helper"
 require "capybara/cuprite"
 
 
-def iniciar_sesion(usuario, clave)
-  visit root_url
-  assert_content 'Pasos de Jesús'
-  click_link 'Iniciar Sesión'
-  assert_content 'Usuario'
-  fill_in('Usuario', with: usuario)
-  fill_in('Clave', with: clave)
-  find_button('Iniciar Sesión').click
-  assert_content 'Sesión iniciada.'
-end
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.javascript_driver = :cuprite
   Capybara.register_driver(:cuprite) do |app|
