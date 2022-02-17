6f62fa720cc767113d2b6c25d3aefa4e6ac739bd
Nombre Usuario <correo@usuario.net>
2022-02-17 11:49:59 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -1056,13 +1056,6 @@ CREATE TABLE public.sip_grupoper (
 
 
 --
--- Name: TABLE sip_grupoper; Type: COMMENT; Schema: public; Owner: -
---
-
-COMMENT ON TABLE public.sip_grupoper IS 'Creado por sip en heb412gen_des';
-
-
---
 -- Name: sip_grupoper_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
