66926c27d4266d09c310f25456b24f0cf4dff4c9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-10 05:06:37 -0500

    Mas pruebas a modelos

---
+++ test/models/mr519_gen/formulario_test.rb
@@ -0,0 +1,28 @@
+require_relative '../../test_helper'
+
+module Mr519Gen
+  class FormularioTest < ActiveSupport::TestCase
+    PRUEBA_FORMULARIO = {
+      nombre:'n',
+      nombreinterno:'n',
+    }
+
+    setup do
+      Rails.application.config.x.formato_fecha = 'yyyy-mm-dd'
+    end
+
+    test "valido" do
+      f = Mr519Gen::Formulario.create PRUEBA_FORMULARIO
+      assert f.valid?
+      f.destroy
+    end
+
+    test "no valido" do
+      f = Mr519Gen::Formulario.new PRUEBA_FORMULARIO
+      f.nombre = nil
+      assert_not f.valid?
+      f.destroy
+    end
+
+  end
+end
