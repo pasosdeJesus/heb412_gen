2083fa923b0eb40c85679e760a69897a4b4e20ce
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-28 11:31:12 -0500

    Eliminado pruebas al sistema como sugiere https://world.hey.com/dhh/system-tests-have-failed-d90af718 Continuaremos pasando a puppeteer

--- test/dummy/test/system/iniciar_sesion_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "application_system_test_case"
-
-class IniciarSesionTest < ApplicationSystemTestCase
-  test "iniciar sesión" do
-    Msip::CapybaraHelper.iniciar_sesion(self, root_path, "heb412", "heb412")
-  end
-end
