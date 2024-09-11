646de48ab6fa6deb63e48d8d9492d88144c42624
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-11 09:25:52 -0500

    Mejora doc

--- app/models/heb412_gen/carpetaexclusiva.rb
+++ app/models/heb412_gen/carpetaexclusiva.rb
@@ -3,7 +3,8 @@
 require "heb412_gen/concerns/models/carpetaexclusiva"
 
 module Heb412Gen
-  # Carpeta exclusiva para administradores en nube
+  # Carpeta exclusiva para administradores en la nube (e.g carpeta con
+  # Respaldos)
   class Carpetaexclusiva < ActiveRecord::Base
     include Heb412Gen::Concerns::Models::Carpetaexclusiva
   end
