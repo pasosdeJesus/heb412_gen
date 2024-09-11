8a1bc28c8f3d27b0748d918535ef0a9adad794a2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-11 09:15:57 -0500

    Documenta

--- app/models/heb412_gen/campohc.rb
+++ app/models/heb412_gen/campohc.rb
@@ -3,6 +3,7 @@
 require "heb412_gen/concerns/models/campohc"
 
 module Heb412Gen
+  # Campo en una plantilla de documento
   class Campohc < ActiveRecord::Base
     include Heb412Gen::Concerns::Models::Campohc
   end
--- app/models/heb412_gen/doc.rb
+++ app/models/heb412_gen/doc.rb
@@ -3,6 +3,7 @@
 require "heb412_gen/concerns/models/doc"
 
 module Heb412Gen
+  # Plantilla de documento
   class Doc < ActiveRecord::Base
     include Heb412Gen::Concerns::Models::Doc
   end
