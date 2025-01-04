bbfaad4d70adeb6eeb3eb02cf482701974bf4ac9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-01-04 16:51:12 -0500

    Actualiza

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
+  config.assets.css_compressor = nil
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
 
